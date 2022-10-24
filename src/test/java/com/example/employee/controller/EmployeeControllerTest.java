250d42d8d0fee241412894bebbf58e2da0e06911
Nika Avalishvili <nika.avalishvili@solveva.com>
2022-10-24 12:31:14 +0400

    Add cleanUp method to integration tests to being it able to run all tests together successfully

--- src/test/java/com/example/employee/controller/EmployeeControllerTest.java
+++ src/test/java/com/example/employee/controller/EmployeeControllerTest.java
@@ -1,12 +1,14 @@
 package com.example.employee.controller;
 
 import com.example.employee.model.EmployeeDTO;
+import com.example.employee.repository.EmployeeRepository;
 import com.example.employee.service.EmployeeService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,18 +32,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class EmployeeControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
 
     @Autowired
-    private EmployeeService employeeService;
+    EmployeeService employeeService;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    ObjectMapper objectMapper;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @BeforeEach
+    void cleanUp() {
+        employeeRepository.deleteAll();
+    }
 
     @Test
     void getAllEmployees() throws Exception {
         EmployeeDTO employeeDTO = new EmployeeDTO.EmployeeDTOBuilder()
-                .id(1L)
                 .first_name("Nika")
                 .last_name("Avalishvili")
                 .department("Business development")
@@ -65,19 +75,16 @@ class EmployeeControllerTest {
         EmployeeDTO expectedEmployeeDto = expectedEmployeeDTOList.stream().findFirst().orElseThrow();
         EmployeeDTO actualEmployeeDto = actualEmployeeDTOList.stream().findFirst().orElseThrow();
 
-        Assertions.assertEquals(expectedEmployeeDto.getFirst_name(), actualEmployeeDto.getFirst_name());
-        Assertions.assertEquals(expectedEmployeeDto.getLast_name(), actualEmployeeDto.getLast_name());
-        Assertions.assertEquals(expectedEmployeeDto.getDepartment(), actualEmployeeDto.getDepartment());
-        Assertions.assertEquals(expectedEmployeeDto.getPositions(), actualEmployeeDto.getPositions());
-        Assertions.assertEquals(expectedEmployeeDto.getEmail(), actualEmployeeDto.getEmail());
-        Assertions.assertEquals(expectedEmployeeDto.getIs_active(), actualEmployeeDto.getIs_active());
-        Assertions.assertEquals(expectedEmployeeDto.getIs_pensions_payer(), actualEmployeeDto.getIs_pensions_payer());
+        assertThat(expectedEmployeeDto)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(actualEmployeeDto);
     }
+
 
     @Test
     void getEmployeeById() throws Exception {
         EmployeeDTO employeeDTO = new EmployeeDTO.EmployeeDTOBuilder()
-                .id(1L)
                 .first_name("Nika")
                 .last_name("Avalishvili")
                 .department("Business development")
@@ -86,21 +93,24 @@ class EmployeeControllerTest {
                 .is_active(true)
                 .is_pensions_payer(true)
                 .buildEmployeeDTO();
-        employeeService.createAndUpdateEmployee(employeeDTO);
+        Long id = employeeService.createAndUpdateEmployee(employeeDTO).getId();
 
-        String responseAsAString = mockMvc.perform(MockMvcRequestBuilders.get("/employee/{id}",1))
+        String responseAsAString = mockMvc.perform(MockMvcRequestBuilders.get("/employee/{id}", id))
                 .andExpect(status().isOk())
                 .andReturn().getResponse().getContentAsString();
 
-        EmployeeDTO actualEmployeeDTO = objectMapper.readValue(responseAsAString, new TypeReference<>(){});
+        EmployeeDTO actualEmployeeDTO = objectMapper.readValue(responseAsAString, new TypeReference<>() {
+        });
 
-        Assertions.assertEquals(employeeDTO, actualEmployeeDTO);
+        assertThat(actualEmployeeDTO)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(employeeDTO);
     }
 
     @Test
     void addOrUpdateEmployee() throws Exception {
         EmployeeDTO employeeDTO = new EmployeeDTO.EmployeeDTOBuilder()
-                .id(1L)
                 .first_name("Harry")
                 .last_name("Potter")
                 .department("Ministry of Magic")
@@ -113,7 +123,7 @@ class EmployeeControllerTest {
 
         objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
-        String requestJson=ow.writeValueAsString(employeeDTO);
+        String requestJson = ow.writeValueAsString(employeeDTO);
 
         String responseAsAString = mockMvc.perform(MockMvcRequestBuilders.post("/employee")
                         .contentType(APPLICATION_JSON)
@@ -121,16 +131,18 @@ class EmployeeControllerTest {
                 .andExpect(status().isOk())
                 .andReturn().getResponse().getContentAsString();
 
-        EmployeeDTO actualEmployeeDTO = objectMapper.readValue(responseAsAString, new TypeReference<>(){});
+        EmployeeDTO actualEmployeeDTO = objectMapper.readValue(responseAsAString, new TypeReference<>() {
+        });
 
-        Assertions.assertEquals(employeeDTO, actualEmployeeDTO);
+        assertThat(actualEmployeeDTO)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(employeeDTO);
     }
-
 
     @Test
     void deleteEmployee() throws Exception {
         EmployeeDTO employeeDTO1 = new EmployeeDTO.EmployeeDTOBuilder()
-                .id(1L)
                 .first_name("Harry")
                 .last_name("Potter")
                 .department("Ministry of Magic")
@@ -140,7 +152,6 @@ class EmployeeControllerTest {
                 .is_pensions_payer(false)
                 .buildEmployeeDTO();
         EmployeeDTO employeeDTO2 = new EmployeeDTO.EmployeeDTOBuilder()
-                .id(2L)
                 .first_name("Albus")
                 .last_name("Dumbledor")
                 .department("Hogwarts")
@@ -149,11 +160,10 @@ class EmployeeControllerTest {
                 .is_active(true)
                 .is_pensions_payer(true)
                 .buildEmployeeDTO();
-        employeeService.createAndUpdateEmployee(employeeDTO1);
+        Long firstId = employeeService.createAndUpdateEmployee(employeeDTO1).getId();
         employeeService.createAndUpdateEmployee(employeeDTO2);
 
-
-        String firstResponseAsAString = mockMvc.perform(MockMvcRequestBuilders.delete("/employee?id={id}",1))
+        String firstResponseAsAString = mockMvc.perform(MockMvcRequestBuilders.delete("/employee?id={id}", firstId))
                 .andExpect(status().isOk())
                 .andReturn().getResponse().getContentAsString();
 
@@ -163,7 +173,13 @@ class EmployeeControllerTest {
         List<EmployeeDTO> actualEmployeeDTOList = objectMapper.readValue(lastResponseAsAString, new TypeReference<>() {
         });
 
-        Assertions.assertNotEquals(employeeDTO1, actualEmployeeDTOList.stream().findFirst().get());
-        Assertions.assertEquals(employeeDTO2, actualEmployeeDTOList.stream().findFirst().get());
+        assertThat(actualEmployeeDTOList)
+                .hasSize(1)
+                .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+                .doesNotContain(employeeDTO1);
+
+        EmployeeDTO actualEmployeeDTO = actualEmployeeDTOList.stream().findFirst().orElseThrow();
+        assertThat(actualEmployeeDTO).usingRecursiveComparison().ignoringFields("id").isEqualTo(employeeDTO2);
+
     }
 }
