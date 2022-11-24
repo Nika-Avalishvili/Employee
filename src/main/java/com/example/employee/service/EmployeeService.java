b8fa8ad95812111e6eaa3f6ea1007560f374ade4
Nika Avalishvili <nika.avalishvili@solveva.com>
2022-11-24 12:31:27 +0400

    Implement RabbitMQ

--- src/main/java/com/example/employee/service/EmployeeService.java
+++ src/main/java/com/example/employee/service/EmployeeService.java
@@ -4,8 +4,10 @@ import com.example.employee.exceptionHandler.EmployeeNotFoundException;
 import com.example.employee.model.Employee;
 import com.example.employee.model.EmployeeDTO;
 import com.example.employee.model.EmployeeMapper;
+import com.example.employee.rabbitMQ.EmployeeConfig;
 import com.example.employee.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,10 +17,12 @@ import java.util.List;
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final EmployeeMapper employeeMapper;
+    private final RabbitTemplate template;
 
     public EmployeeDTO createAndUpdateEmployee(EmployeeDTO employeeDTO) {
         Employee employee = employeeMapper.dtoToEntity(employeeDTO);
         employeeRepository.save(employee);
+        template.convertAndSend(EmployeeConfig.EXCHANGE, EmployeeConfig.ROUTING_KEY, employeeDTO);
         return employeeMapper.entityToDto(employee);
     }
 
